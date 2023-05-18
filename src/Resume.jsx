014acc618c016204b0431154e0f04a8303844f0a
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-05-18 07:51:14 +0100

    Add files via upload

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { FaDownload } from 'react-icons/fa'
 const file = require("./Docs/RESUME.pdf");
+const file2 = require("./Docs/CV.pdf");
 
 function Resume() {
   return (
     <div className='resume'>
-         <a href={file} download >
-            <button className="btn  btn-downlaod"><FaDownload/> DOWNLOAD NOW</button>
+        <div className='block'>
+        <a href={file} download >
+            <button className="btn  btn-downlaod"><FaDownload/> Download - English Version</button>
          </a>
+           <br />
+           <a href={file2} download >
+            <button className="btn  btn-downlaod"><FaDownload/> Download - Portuguese Version</button>
+         </a>
+        </div>
     </div>
   )
 }
